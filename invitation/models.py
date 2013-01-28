e83e573dd35fb0111271293f5ca12f195c5d91b7
Dirk Eschler <eschler@gmail.com>
2013-01-28 11:33:52 +0100

    Handled timezone-aware datetimes.

--- invitation/models.py
+++ invitation/models.py
@@ -10,6 +10,7 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.contrib.sites.models import Site
+from django.utils.timezone import utc
 
 from registration.models import SHA1_RE
 
@@ -21,14 +22,14 @@ class InvitationKeyManager(models.Manager):
         # Don't bother hitting database if invitation_key doesn't match pattern.
         if not SHA1_RE.search(invitation_key):
             return None
-        
+
         try:
             key = self.get(key=invitation_key)
         except self.model.DoesNotExist:
             return None
-        
+
         return key
-        
+
     def is_key_valid(self, invitation_key):
         """
         Check if an ``InvitationKey`` is valid or not, returning a boolean,
@@ -40,8 +41,8 @@ class InvitationKeyManager(models.Manager):
     def create_invitation(self, user):
         """
         Create an ``InvitationKey`` and returns it.
-        
-        The key for the ``InvitationKey`` will be a SHA1 hash, generated 
+
+        The key for the ``InvitationKey`` will be a SHA1 hash, generated
         from a combination of the ``User``'s username and a random salt.
         """
         salt = sha_constructor(str(random.random())).hexdigest()[:5]
@@ -65,67 +66,71 @@ class InvitationKeyManager(models.Manager):
 
 class InvitationKey(models.Model):
     key = models.CharField(_('invitation key'), max_length=40)
-    date_invited = models.DateTimeField(_('date invited'), 
+    date_invited = models.DateTimeField(_('date invited'),
                                         default=datetime.datetime.now)
-    from_user = models.ForeignKey(User, 
+    from_user = models.ForeignKey(User,
                                   related_name='invitations_sent')
-    registrant = models.ForeignKey(User, null=True, blank=True, 
+    registrant = models.ForeignKey(User, null=True, blank=True,
                                   related_name='invitations_used')
-    
+
     objects = InvitationKeyManager()
-    
+
     def __unicode__(self):
         return u"Invitation from %s on %s" % (self.from_user.username, self.date_invited)
-    
+
     def is_usable(self):
         """
-        Return whether this key is still valid for registering a new user.        
+        Return whether this key is still valid for registering a new user.
         """
         return self.registrant is None and not self.key_expired()
-    
+
     def key_expired(self):
         """
-        Determine whether this ``InvitationKey`` has expired, returning 
+        Determine whether this ``InvitationKey`` has expired, returning
         a boolean -- ``True`` if the key has expired.
-        
-        The date the key has been created is incremented by the number of days 
-        specified in the setting ``ACCOUNT_INVITATION_DAYS`` (which should be 
+
+        The date the key has been created is incremented by the number of days
+        specified in the setting ``ACCOUNT_INVITATION_DAYS`` (which should be
         the number of days after invite during which a user is allowed to
-        create their account); if the result is less than or equal to the 
+        create their account); if the result is less than or equal to the
         current date, the key has expired and this method returns ``True``.
-        
+
         """
         expiration_date = datetime.timedelta(days=settings.ACCOUNT_INVITATION_DAYS)
-        return self.date_invited + expiration_date <= datetime.datetime.now()
+        if settings.USE_TZ:
+            now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        else:
+            now = datetime.datetime.now()
+        return self.date_invited + expiration_date <= now
     key_expired.boolean = True
-    
+
     def mark_used(self, registrant):
         """
         Note that this key has been used to register a new user.
         """
         self.registrant = registrant
         self.save()
-        
+
     def send_to(self, email):
         """
         Send an invitation email to ``email``.
         """
         current_site = Site.objects.get_current()
-        
+
         subject = render_to_string('invitation/invitation_email_subject.txt',
-                                   { 'site': current_site, 
+                                   { 'site': current_site,
                                      'invitation_key': self })
         # Email subject *must not* contain newlines
         subject = ''.join(subject.splitlines())
-        
+
         message = render_to_string('invitation/invitation_email.txt',
                                    { 'invitation_key': self,
                                      'expiration_days': settings.ACCOUNT_INVITATION_DAYS,
                                      'site': current_site })
-        
+
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
 
-        
+
 class InvitationUser(models.Model):
     inviter = models.ForeignKey(User, unique=True)
     invitations_remaining = models.IntegerField()
@@ -133,7 +138,7 @@ class InvitationUser(models.Model):
     def __unicode__(self):
         return u"InvitationUser for %s" % self.inviter.username
 
-    
+
 def user_post_save(sender, instance, created, **kwargs):
     """Create InvitationUser for user when User is created."""
     if created:
